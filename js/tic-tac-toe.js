438e0589c9efda8fed10f3fb8e5fb50938571fe0
Kinglightning07 <chase.e.freedman@gmail.com>
2024-08-25 13:12:27 -0400

    added space to win alert

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -87,7 +87,7 @@ function set() {
     moves += 1;
     score[turn] += this.identifier;
     if (win(this)) {
-        alert(turn + "won the game!");
+        alert(turn + " " + "won the game!");
         startNewGame();
     } else if (moves === N_SIZE * N_SIZE) {
         alert('Draw');
